2165ffe0fd23452c6c2d7358fe4f480a52b75b96
Raluca Nicola <RNicola@esri.com>
2022-11-03 14:02:23 +0100

    Style changes

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -3,10 +3,6 @@ import { Title } from '../index';
 const Background = ({ title, size, children }) => {
   return (
     <div className={styles.background}>
-      <div className={styles.borderLeftTop}></div>
-      <div className={styles.borderRightTop}></div>
-      <div className={styles.borderRightBottom}></div>
-      <div className={styles.borderLeftBottom}></div>
       <Title text={title} size={size}></Title>
       <div className={styles.scroll}>{children}</div>
     </div>
--- src/components/MeasurementsPanel/MeasurementsPanel.jsx
+++ src/components/MeasurementsPanel/MeasurementsPanel.jsx
@@ -1,34 +1,39 @@
-import {Background} from "../index";
-import * as styles from "./MeasurementsPanel.module.css";
-import "@esri/calcite-components/dist/components/calcite-label";
-import "@esri/calcite-components/dist/components/calcite-radio-button-group";
-import "@esri/calcite-components/dist/components/calcite-radio-button";
-import "@esri/calcite-components/dist/components/calcite-switch";
-import {CalciteLabel, CalciteRadioButtonGroup, CalciteRadioButton, CalciteSwitch} from "@esri/calcite-components-react";
-import {variables} from "../../config";
-import {useEffect, useRef} from "react";
+import { Background } from '../index';
+import * as styles from './MeasurementsPanel.module.css';
+import '@esri/calcite-components/dist/components/calcite-label';
+import '@esri/calcite-components/dist/components/calcite-radio-button-group';
+import '@esri/calcite-components/dist/components/calcite-radio-button';
+import '@esri/calcite-components/dist/components/calcite-switch';
+import {
+  CalciteLabel,
+  CalciteRadioButtonGroup,
+  CalciteRadioButton,
+  CalciteSwitch
+} from '@esri/calcite-components-react';
+import { variables } from '../../config';
+import { useEffect, useRef } from 'react';
 
-const MeasurementsPanel = ({selectedVariable, setSelectedVariable, setLegendContainer}) => {
+const MeasurementsPanel = ({ selectedVariable, setSelectedVariable, setLegendContainer }) => {
   const legendContainerRef = useRef();
   useEffect(() => {
     setLegendContainer(legendContainerRef.current);
   }, [legendContainerRef]);
   return (
-    <Background title="Variables" size="small">
+    <Background title='Variables' size='small'>
       <CalciteRadioButtonGroup
-        name="pressure-group"
-        layout="vertical"
-        scale="s"
-        onCalciteRadioButtonChange={event => {
-          const variable = variables.filter(v => v.name === event.target.value)[0];
+        name='pressure-group'
+        layout='vertical'
+        scale='s'
+        onCalciteRadioButtonChange={(event) => {
+          const variable = variables.filter((v) => v.name === event.target.value)[0];
           setSelectedVariable(variable);
         }}
       >
         {variables.map((variable, index) => {
-          const checked = selectedVariable.name === variable.name ? {checked: true} : undefined;
+          const checked = selectedVariable.name === variable.name ? { checked: true } : undefined;
           return (
-            <CalciteLabel key={index} layout="inline" className={styles.label}>
-              <CalciteRadioButton value={variable.name} {...checked} scale="s"></CalciteRadioButton>
+            <CalciteLabel key={index} layout='inline' className={styles.label}>
+              <CalciteRadioButton value={variable.name} {...checked} scale='s'></CalciteRadioButton>
               {variable.name}
             </CalciteLabel>
           );
@@ -37,6 +42,9 @@ const MeasurementsPanel = ({selectedVariable, setSelectedVariable, setLegendCont
 
       <div className={styles.variableInfo}>
         <div ref={legendContainerRef}></div>
+      </div>
+      <div className={styles.overviewMap}>
+        <img src='./assets/overview-map.png'></img>
       </div>
     </Background>
   );
