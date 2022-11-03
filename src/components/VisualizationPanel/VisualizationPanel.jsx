334bd4523736f8ff8b9852639898583f7a0abe72
Raluca Nicola <RNicola@esri.com>
2022-11-03 16:01:31 +0100

    Fix south north slider

--- src/components/VisualizationPanel/VisualizationPanel.jsx
+++ src/components/VisualizationPanel/VisualizationPanel.jsx
@@ -1,34 +1,34 @@
-import {Background} from "../index";
-import * as styles from "./VisualizationPanel.module.css";
-import "@esri/calcite-components/dist/components/calcite-slider";
-import "@esri/calcite-components/dist/components/calcite-switch";
-import "@esri/calcite-components/dist/components/calcite-label";
-import "@esri/calcite-components/dist/components/calcite-radio-button-group";
-import "@esri/calcite-components/dist/components/calcite-radio-button";
+import { Background } from '../index';
+import * as styles from './VisualizationPanel.module.css';
+import '@esri/calcite-components/dist/components/calcite-slider';
+import '@esri/calcite-components/dist/components/calcite-switch';
+import '@esri/calcite-components/dist/components/calcite-label';
+import '@esri/calcite-components/dist/components/calcite-radio-button-group';
+import '@esri/calcite-components/dist/components/calcite-radio-button';
 import {
   CalciteLabel,
   CalciteRadioButtonGroup,
   CalciteRadioButton,
   CalciteSlider,
-  CalciteSwitch,
-} from "@esri/calcite-components-react";
-import {useEffect, useState} from "react";
-import VoxelSlice from "@arcgis/core/layers/voxel/VoxelSlice";
+  CalciteSwitch
+} from '@esri/calcite-components-react';
+import { useEffect, useState } from 'react';
+import VoxelSlice from '@arcgis/core/layers/voxel/VoxelSlice';
 
 const getSlice = (orientation, value) => {
   switch (orientation) {
-    case "NS":
+    case 'NS':
       return new VoxelSlice({
         orientation: 180,
         tilt: 90,
-        point: [0, value, 0],
+        point: [0, value, 0]
       });
-    case "WE":
-      return new VoxelSlice({orientation: 270, tilt: 90, point: [value, 0, 0]});
-    case "UD":
+    case 'WE':
+      return new VoxelSlice({ orientation: 270, tilt: 90, point: [value, 0, 0] });
+    case 'UD':
       return new VoxelSlice({
         tilt: 0,
-        point: [0, 0, value],
+        point: [0, 0, value]
       });
   }
 };
@@ -52,7 +52,7 @@ const VisualizationPanel = ({
   displaySlices,
   setDisplaySlices,
   setSlices,
-  dimensions,
+  dimensions
 }) => {
   const [currentNSSection, setCurrentNSSection] = useState(null);
   const [currentWESection, setCurrentWESection] = useState(null);
@@ -62,7 +62,7 @@ const VisualizationPanel = ({
 
   useEffect(() => {
     if (displaySections && currentNSSection && currentWESection) {
-      let updatedSections = sections.map(section => {
+      let updatedSections = sections.map((section) => {
         if (section.label === `ns${currentNSSection}` || section.label === `we${currentWESection}`) {
           section.enabled = true;
         } else {
@@ -76,9 +76,9 @@ const VisualizationPanel = ({
 
   useEffect(() => {
     if (dimensions) {
-      setCurrentWESlice(getSlice("WE", dimensions[0]));
-      setCurrentNSSlice(getSlice("NS", 1));
-      setCurrentUpDownSlice(getSlice("UD", dimensions[2]));
+      setCurrentWESlice(getSlice('WE', dimensions[0]));
+      setCurrentNSSlice(getSlice('NS', 1));
+      setCurrentUpDownSlice(getSlice('UD', dimensions[2]));
       setCurrentNSSection(Math.floor(dimensions[0] / 2));
       setCurrentWESection(Math.floor(dimensions[1] / 2));
     }
@@ -91,186 +91,186 @@ const VisualizationPanel = ({
   }, [currentNSSlice, currentWESlice, currentUpDownSlice, displaySlices]);
 
   return (
-    <Background title="Visualization" size="small">
+    <Background title='Visualization' size='small'>
       <CalciteRadioButtonGroup
-        name="visualization-group"
-        layout="vertical"
-        scale="s"
-        onCalciteRadioButtonChange={event => {
+        name='visualization-group'
+        layout='vertical'
+        scale='s'
+        onCalciteRadioButtonChange={(event) => {
           setSelectedVisualization(event.target.value);
         }}
       >
-        <CalciteLabel layout="inline" className={styles.label}>
+        <CalciteLabel layout='inline' className={styles.label}>
           <CalciteRadioButton
-            value="volume"
-            checked={selectedVisualization === "volume" ? true : undefined}
-            scale="s"
+            value='volume'
+            checked={selectedVisualization === 'volume' ? true : undefined}
+            scale='s'
           ></CalciteRadioButton>
           Full volume
         </CalciteLabel>
-        <CalciteLabel layout="inline" className={styles.label}>
+        <CalciteLabel layout='inline' className={styles.label}>
           <CalciteRadioButton
-            value="surfaces"
-            checked={selectedVisualization === "surfaces" ? true : undefined}
-            scale="s"
+            value='surfaces'
+            checked={selectedVisualization === 'surfaces' ? true : undefined}
+            scale='s'
           ></CalciteRadioButton>
           Surfaces and sections
         </CalciteLabel>
       </CalciteRadioButtonGroup>
-      {selectedVisualization === "surfaces" && continuousVariable && isosurfaceInfo ? (
+      {selectedVisualization === 'surfaces' && continuousVariable && isosurfaceInfo ? (
         <div className={styles.surfaces}>
           <CalciteLabel
             className={styles.label}
-            layout="inline-space-between"
-            onCalciteSwitchChange={event => {
+            layout='inline-space-between'
+            onCalciteSwitchChange={(event) => {
               setDisplayIsosurface(event.target.checked);
             }}
           >
             Display isosurface ({selectedVariable.unit})
-            <CalciteSwitch scale="m" checked={displayIsosurface ? true : undefined}></CalciteSwitch>
+            <CalciteSwitch scale='m' checked={displayIsosurface ? true : undefined}></CalciteSwitch>
           </CalciteLabel>
           {displayIsosurface ? (
             <>
-              <img className={styles.surfaceGraphic} src="./assets/surface.png"></img>
+              <img className={styles.surfaceGraphic} src='./assets/surface.png'></img>
               <CalciteSlider
                 labelHandles
                 min={isosurfaceInfo.min}
                 max={isosurfaceInfo.max}
-                scale="m"
+                scale='m'
                 value={isosurfaceValue}
                 snap
                 step={isosurfaceInfo.max - isosurfaceInfo.min < 10 ? 0.2 : 1}
-                onCalciteSliderInput={event => {
+                onCalciteSliderInput={(event) => {
                   const value = event.target.value;
                   setIsosurfaceValue(value);
                 }}
               ></CalciteSlider>
             </>
           ) : (
-            ""
+            ''
           )}
         </div>
       ) : (
-        ""
+        ''
       )}
-      {selectedVisualization === "surfaces" ? (
+      {selectedVisualization === 'surfaces' ? (
         <div className={styles.dynamicSections}>
           <CalciteLabel
             className={styles.label}
-            layout="inline-space-between"
-            onCalciteSwitchChange={event => {
+            layout='inline-space-between'
+            onCalciteSwitchChange={(event) => {
               setDisplaySections(event.target.checked);
             }}
           >
             Display sections
-            <CalciteSwitch scale="m" checked={displaySections ? true : undefined}></CalciteSwitch>
+            <CalciteSwitch scale='m' checked={displaySections ? true : undefined}></CalciteSwitch>
           </CalciteLabel>
           {displaySections && dimensions.length > 0 ? (
             <div className={styles.sections}>
-              <img className={styles.surfaceGraphic} src="./assets/section-north-south.png"></img>
+              <img className={styles.surfaceGraphic} src='./assets/section-north-south.png'></img>
               <CalciteSlider
                 min={1}
                 max={dimensions[0]}
-                scale="m"
+                scale='m'
                 value={currentNSSection}
                 snap
-                step="1"
-                onCalciteSliderInput={event => {
+                step='1'
+                onCalciteSliderInput={(event) => {
                   const value = event.target.value;
                   setCurrentNSSection(value);
                 }}
               ></CalciteSlider>
 
-              <img className={styles.surfaceGraphic} src="./assets/section-west-east.png"></img>
+              <img className={styles.surfaceGraphic} src='./assets/section-west-east.png'></img>
               <CalciteSlider
                 min={1}
                 max={dimensions[1]}
-                scale="m"
+                scale='m'
                 value={currentWESection}
                 snap
-                step="1"
-                onCalciteSliderInput={event => {
+                step='1'
+                onCalciteSliderInput={(event) => {
                   const value = event.target.value;
                   setCurrentWESection(value);
                 }}
               ></CalciteSlider>
             </div>
           ) : (
-            ""
+            ''
           )}
         </div>
       ) : (
-        ""
+        ''
       )}
       {/* <div className="separator"></div> */}
       <div>
         <CalciteLabel
           className={styles.label}
-          layout="inline-space-between"
-          onCalciteSwitchChange={event => {
+          layout='inline-space-between'
+          onCalciteSwitchChange={(event) => {
             setDisplaySlices(event.target.checked);
           }}
         >
           Slice layer
-          <CalciteSwitch scale="m" checked={displaySlices ? true : undefined}></CalciteSwitch>
+          <CalciteSwitch scale='m' checked={displaySlices ? true : undefined}></CalciteSwitch>
         </CalciteLabel>
         {displaySlices && dimensions.length > 0 ? (
           <div className={styles.slices}>
-            <img className={styles.sliceGraphic} src="./assets/slice-west-east.png"></img>
+            <img className={styles.sliceGraphic} src='./assets/slice-west-east.png'></img>
             <CalciteSlider
               min={1}
               max={dimensions[0]}
-              scale="m"
+              scale='m'
               value={currentWESlice.point[0]}
               snap
               step={1}
-              onCalciteSliderInput={event => {
+              onCalciteSliderInput={(event) => {
                 const value = event.target.value;
-                setCurrentWESlice(getSlice("WE", value));
+                setCurrentWESlice(getSlice('WE', value));
               }}
             ></CalciteSlider>
-            <img className={styles.sliceGraphic} src="./assets/slice-north-south.png"></img>
+            <img className={styles.sliceGraphic} src='./assets/slice-north-south.png'></img>
             <CalciteSlider
               min={1}
               max={dimensions[1] - 2}
-              scale="m"
-              value={currentNSSlice.point[1]}
+              scale='m'
+              value={dimensions[1] - 2 - currentNSSlice.point[1]}
               snap
               step={1}
-              onCalciteSliderInput={event => {
-                const value = event.target.value;
-                setCurrentNSSlice(getSlice("NS", value));
+              onCalciteSliderInput={(event) => {
+                const value = dimensions[1] - 2 - event.target.value;
+                setCurrentNSSlice(getSlice('NS', value));
               }}
             ></CalciteSlider>
-            <img className={styles.sliceGraphic} src="./assets/slice-up-down.png"></img>
+            <img className={styles.sliceGraphic} src='./assets/slice-up-down.png'></img>
             <CalciteSlider
               min={0}
               max={dimensions[2]}
-              scale="m"
+              scale='m'
               value={currentUpDownSlice.point[2]}
               snap
               step={1}
-              onCalciteSliderInput={event => {
+              onCalciteSliderInput={(event) => {
                 const value = event.target.value;
-                setCurrentUpDownSlice(getSlice("UD", value));
+                setCurrentUpDownSlice(getSlice('UD', value));
               }}
             ></CalciteSlider>
           </div>
         ) : (
-          ""
+          ''
         )}
       </div>
       {/* <div className="separator"></div> */}
       <CalciteLabel className={styles.label}>Vertical exaggeration</CalciteLabel>
       <CalciteSlider
         labelHandles
-        min="1"
-        max="20"
-        scale="m"
+        min='1'
+        max='20'
+        scale='m'
         value={exaggeration}
         snap
-        step="1"
-        onCalciteSliderInput={event => {
+        step='1'
+        onCalciteSliderInput={(event) => {
           const value = event.target.value ? event.target.value : 1;
           setExaggeration(value);
         }}
